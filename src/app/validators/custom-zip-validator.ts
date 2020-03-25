1d485ad355dc7c4e87c2e8f155bd966b1ba74abb
Jovan Nakov <Jovan.Nakov@endava.com>
2020-03-25 23:30:00 +0100

    Implemented another function for validating the zip codes

--- src/app/validators/custom-zip-validator.ts
+++ src/app/validators/custom-zip-validator.ts
@@ -10,3 +10,14 @@ export function ValidateZipCode(control: AbstractControl) {
     });
     return {validZip:false,city:null};
 }
+
+// Found it dificult to implement the first one so i made another one, i found this one simpler to implement
+export function ZipCodeToCityValidator(zip:any){
+    let city = {validZip : false, city:"The city doesn't exist in our database!"};
+    City.forEach(zipObj => {
+        if(zipObj.zip == zip){
+            city = {validZip : true, city:zipObj.city};
+        }
+    });
+    return city;
+}
